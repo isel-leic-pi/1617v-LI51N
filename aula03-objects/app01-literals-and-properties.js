40568ca0f8bb2f688a9038a7173c088207780544
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-03-07 22:55:42 +0000

    aula 3

---
+++ aula03-objects/app01-literals-and-properties.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/*
+const std1 = {
+    nr: 73648,
+    name: 'Ze Manel',
+    course: 'LEIC'  
+}
+*/
+function makeStudent(nr, name, course) {
+    return {
+        'nr': nr,
+        'name': name,
+        'course': course
+    }
+}
+
+function reflectProps(obj) {
+    // Type klass = std1.GetType()
+    for (let key in obj) {
+        console.log(key + ' = ' + obj[key])
+        // klass.GetField(key).GetValue(std1)
+    }    
+}
+
+const std1 = makeStudent(8478, 'Ze Manel', 'Leic')
+const std2 = makeStudent(7386, 'Maria Papoila', 'LEIC')
+std1.school = 'ISEL'
+
+reflectProps(std1)
+reflectProps(std2)
