d5aab397f34858916a8b5e65ce72a0a765ec14fe
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-03-28 21:26:06 +0100

    Handle Server Errors (status 500)

--- aula11-web-app-football/footballWebApp.js
+++ aula11-web-app-football/footballWebApp.js
@@ -8,28 +8,35 @@ const port = process.argv[2] | 3000
 /**
  * Init HTTP server
  */
-const server = http.createServer(callback)
+const server = http.createServer(httpServerListener)
 server.listen(port)
 console.log('Listening on port ' + port)
 
-function callback(req, resp) {
+function httpServerListener(req, resp) {
     const urlObj = url.parse(req.url, true)
     const parts = urlObj.pathname.split('/')
     const endpoint = parts[2]
     let action = football[endpoint]
     if(action == undefined || parts[1] != 'football') {
-        resp.statusCode = 404
-        resp.end()
+        sendResponse(404)
     } else {
         /**
          * 1. Call action
          * 2. Representação: Obter uma String com a representação JSON do recurso.
          * 3. Envio da resposta: statusCode 200 + send() + end()
          */
-        const obj = action(urlObj.query.iso)
-        const data = JSON.stringify(obj)
-        resp.writeHead(200, { 'Content-Type': 'application/json' })
-        resp.end(data)
+        action((err, obj) => {
+            if(err) sendResponse(500, err.message)
+            const data = JSON.stringify(obj)
+            resp.writeHead(200, { 'Content-Type': 'application/json' })
+            resp.end(data)
+        })
+    }
+
+    function sendResponse(status, msg) {
+        resp.statusCode = status
+        resp.write(msg)
+        resp.end()
     }
 }
 
