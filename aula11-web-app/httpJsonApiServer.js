da2c81f300a40d377396e5dead05a2cf84b17d4a
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-03-28 20:30:06 +0100

    Replace if/else block by selection based on property name

--- aula11-web-app/httpJsonApiServer.js
+++ aula11-web-app/httpJsonApiServer.js
@@ -11,25 +11,11 @@ const port = process.argv[2]
 const server = http.createServer(callback)
 server.listen(port)
 
-/**
- * Endpoints paths
- */
-const parseTime = '/api/parsetime'
-const unixTime = '/api/unixtime'
-const dateTime = '/api/datetime'
 
 function callback(req, resp) {
     const urlObj = url.parse(req.url, true)
-    let action
-    if (urlObj.pathname == parseTime) {
-        action = timeService.parsetime
-    }
-    else if (urlObj.pathname == unixTime) {
-        action = timeService.unixtime
-    }
-    else if (urlObj.pathname == dateTime) {
-        action = timeService.datetime
-    }
+    const endpoint = urlObj.pathname.split('/')[2]
+    let action = timeService[endpoint]
     if(action != undefined) {
         /**
          * 1. Call action
