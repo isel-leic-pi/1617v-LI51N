815154fc0ee808558f90feafb96f2c4a2506b0d6
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-04-04 12:18:23 +0100

    Unit test using a mock request to json file

--- aula13-IoC/test/testFootballService.js
+++ aula13-IoC/test/testFootballService.js
@@ -1,19 +1,19 @@
 'use strict'
 
-const req = require('request')
+const req = require('./requestFile')
 const football = require('./../footballService')(req)
 
 module.exports.testGetLeagues = function(test) {
-
     football.getLeagues((err, arrLeagues) => {
+        if(err) return failure(err, test)
         test.equal(arrLeagues[1].caption, 'Premier League 2016/17')
         test.done()
     })
 }
 
 module.exports.testGetLeagueTable = function(test) {
-
     football.getLeagueTable(426, (err, leagueTable) => {
+        if(err) return failure(err, test)
         test.equal(leagueTable.caption, 'Premier League 2016/17')
         test.equal(leagueTable.teams[0].name, 'Chelsea FC')
         test.equal(leagueTable.teams.length, 20)
@@ -21,3 +21,8 @@ module.exports.testGetLeagueTable = function(test) {
     })
 }
 
+function failure(err, test) {
+    console.log(err.message)
+    test.ok(false)
+    test.done()
+}
