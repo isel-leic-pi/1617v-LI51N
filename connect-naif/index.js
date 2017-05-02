bb17a2c3c34ae1f981c5eb960e06c2fbae0beca2
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-05-02 22:27:11 +0100

    Finish connect-naif implementation

--- connect-naif/index.js
+++ connect-naif/index.js
@@ -1,21 +1,21 @@
-module.exports = initStak
+module.exports = initStack
 
 function initStack() {
     /**
      * 1. Stack de Middlewares
      */
     const mws = []
-    
     /**
      * 2. Objecto função (req, resp) => void que será o handler
      * do servidor HTTP. e.g. http.createServer(app)
      */
-    const handler = (req, resp) => {
-        if(mws.length > 0)
-            mws[0](req, res, next)
-    }
-    function next(){
-        // TO DO: Chamar o próximo enquanto existir
+    const handler = (req, res) => {
+        let index = 0
+        next()
+        function next(){
+            if(index < mws.length)
+                mws[index++](req, res, next)
+        }
     }
     /**
      * 3. Método use() adiciona o mw à pilha.
