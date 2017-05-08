efc7e104a29bf9142645590d4fe705af957e025d
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-05-08 19:55:30 +0100

    Replace var by const. Replace routes by fottballCtr. Register partial views.

--- aula22-express-generator/football/app.js
+++ aula22-express-generator/football/app.js
@@ -1,18 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const hbs = require('hbs')
+const footballCtr = require('./controller/footballController')
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,9 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
+addFootbalRoutes(app)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,3 +41,23 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+/**
+ * Adds a route for each method of footballCtr object.
+ */
+function addFootbalRoutes(app) {
+    for (let endpoint in footballCtr) {
+        const action = footballCtr[endpoint]
+        app.get('/football/' + endpoint, (req, res, next) => {
+            /**
+             * 1. Call action with query-string arguments
+             * 2. Representação: Obter uma String com a representação HTML do recurso.
+             * 3. Envio da resposta: statusCode 200 + send() + end()
+             */
+            action(req.query, (err, data) => {
+                if(err) return next(err)
+                res.render(endpoint, data)
+            })
+        })
+    }
+}
