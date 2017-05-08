a9212817e90a138b9cb7d1bdb3ce9f3eb7b63af7
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-05-08 19:40:29 +0100

    Replace connect by express. Use express routing

--- aula22-express/controller/footballController.js
+++ aula22-express/controller/footballController.js
@@ -1,41 +1,17 @@
 'use strict'
 
 const football = require('./../footballService')
-const fs = require('fs')
-const handlebars = require('handlebars')
-const util = require('util')
 
 module.exports = {
     leagues,
     leagueTable
 }
 
-const leaguesView = handlebars.compile(
-    fs.readFileSync('./views/leagues.hbs').toString())
-const leagueTableView = handlebars.compile(
-    fs.readFileSync('./views/leagueTable.hbs').toString())
-
-handlebars.registerPartial('leagueLink', 
-    fs.readFileSync('./views/partials/leagueLink.hbs').toString())
-
-handlebars.registerPartial('layout', 
-    fs.readFileSync('./views/partials/layout.hbs').toString())
-
-/*
-handlebars.registerHelper('linkForLeague', linkForLeague)
-function linkForLeague(id, caption){
-    return  util.format(
-        '<a href="leagueTable?id=%s">%s</a>', 
-        id,
-        caption)
-}
-*/
-
 function leagues(query, cb) {
     football.getLeagues((err, leagues) => {
         if(err) cb(err)
         leagues.title = 'Leagues'
-        cb(null, leaguesView(leagues))
+        cb(null, leagues)
     })
 }
 
@@ -44,6 +20,6 @@ function leagueTable(query, cb) {
     football.getLeagueTable(query.id, (err, leagueTable) => {
         if(err) return cb(err)
         leagueTable.title = 'League Table'
-        cb(null, leagueTableView(leagueTable))
+        cb(null,leagueTable)
     })
 }
--- aula22-express/footballWebApp.js
+++ aula22-express/footballWebApp.js
@@ -1,21 +1,19 @@
 const http = require('http')
-const connect = require('./../connect-naif')
+const express = require('express')
 const serveStatic = require('serve-static')
-const url = require('url')
-const fs = require('fs')
 const footballCtr = require('./controller/footballController')
-
+const hbs = require('hbs')
 
 const port = process.argv[2] | 3000
-const app = connect()
+const app = express()
+app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials')
 
 /**
  * Build connect Middlewares stack
  */
-app.use(parsePath)
-// app.use(readCss)
 app.use(serveStatic('public'))
-app.use(footballAction)
+addFootbalRoutes(app)
 app.use((req, resp) => sendResponse(resp, 404, 'Resource not Found'))
 app.use((err, req, resp, next) => sendResponse(resp, 500, err.message))
 
@@ -26,42 +24,22 @@ const server = http.createServer(app)
 server.listen(port)
 console.log('Listening on port ' + port)
 
-
-function parsePath(req, resp, next) {
-    const urlObj = url.parse(req.url, true)
-    req.pathname = urlObj.pathname
-    req.query = urlObj.query
-    next()
-}
-
-function readCss(req, resp, next) {
-    if(req.pathname.indexOf('.css') > 0) {
-        const file = req.pathname.substring(10)
-        fs.readFile(file, (err, data) => {
-            if(err) next(err)
-            else sendResponse(resp, 200, data.toString())
-        })
-    } else {
-        next()
-    }
-}
-
-function footballAction(req, resp, next) {
-    const parts = req.pathname.split('/')
-    const endpoint = parts[2]
-    let action = footballCtr[endpoint]
-    if(action == undefined || parts[1] != 'football') {
-        return next()
-    } else {
-        /**
-         * 1. Call action with query-string arguments
-         * 2. Representação: Obter uma String com a representação JSON do recurso.
-         * 3. Envio da resposta: statusCode 200 + send() + end()
-         */
-        action(req.query, (err, data) => {
-            if(err) return next(err)
-            resp.writeHead(200, { 'Content-Type': 'text/html' })
-            resp.end(data)
+/**
+ * Adds a route for each method of footballCtr object.
+ */
+function addFootbalRoutes(app) {
+    for (let endpoint in footballCtr) {
+        const action = footballCtr[endpoint]
+        app.get('/football/' + endpoint, (req, res, next) => {
+            /**
+             * 1. Call action with query-string arguments
+             * 2. Representação: Obter uma String com a representação HTML do recurso.
+             * 3. Envio da resposta: statusCode 200 + send() + end()
+             */
+            action(req.query, (err, data) => {
+                if(err) return next(err)
+                res.render(endpoint, data)
+            })
         })
     }
 }
