784bb16002e206dd4897bf5337e3eadb332a66de
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-05-09 22:11:58 +0100

    Add generic buildRoutes()

--- aula23-forms/controller/footballController.js
+++ aula23-forms/controller/footballController.js
@@ -4,10 +4,11 @@ const football = require('./../footballService')
 
 module.exports = {
     leagues,
-    leagueTable
+    getLeagueTable,
+    postDummy
 }
 
-function leagues(query, cb) {
+function leagues(req, cb) {
     football.getLeagues((err, leagues) => {
         if(err) cb(err)
         leagues.title = 'Leagues'
@@ -15,11 +16,15 @@ function leagues(query, cb) {
     })
 }
 
-function leagueTable(query, cb) {
-    if(!query.id) return cb(new Error('Id not found'))
-    football.getLeagueTable(query.id, (err, leagueTable) => {
-        if(err) return cb(err)
+function getLeagueTable(req, res, next) {
+    if(!req.query.id) return next(new Error('Id not found'))
+    football.getLeagueTable(req.query.id, (err, leagueTable) => {
+        if(err) return next(err)
         leagueTable.title = 'League Table'
-        cb(null,leagueTable)
+        res.render('leagueTable', leagueTable)
     })
+}
+
+function postDummy(req, res) {
+    res.send(200)
 }
