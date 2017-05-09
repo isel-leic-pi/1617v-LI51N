a39b04320621102ddd9736b1bc7833bb9aaacbca
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-05-09 22:51:54 +0100

    forms

--- aula23-forms/controller/footballController.js
+++ aula23-forms/controller/footballController.js
@@ -1,10 +1,13 @@
 'use strict'
 
 const football = require('./../footballService')
+const League = require('./../model/League')
 
 module.exports = {
     leagues,
     getLeagueTable,
+    favourites,
+    postAddFavourite,
     postDummy
 }
 
@@ -12,6 +15,7 @@ function leagues(req, cb) {
     football.getLeagues((err, leagues) => {
         if(err) cb(err)
         leagues.title = 'Leagues'
+        leagues.showAddFavourites = true
         cb(null, leagues)
     })
 }
@@ -23,6 +27,18 @@ function getLeagueTable(req, res, next) {
         leagueTable.title = 'League Table'
         res.render('leagueTable', leagueTable)
     })
+}
+
+const favouritesList = []
+
+function favourites(req, res) {
+    res.render('leagues', favouritesList)
+}
+
+function postAddFavourite(req, res, next) {
+    if(!req.body.id) next(new Error('Missing id on body!!s'))
+    favouritesList.push(new League(req.body))
+    res.redirect('/football/favourites')
 }
 
 function postDummy(req, res) {
