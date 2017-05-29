686bf08d524a35090885915b7bef21c9e275ebf3
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-05-29 19:44:31 +0100

    dom

---
+++ aula31-dom/app03.js
@@ -0,0 +1,26 @@
+ window.onload = () => {
+    const btAdd = document.querySelector('#btAdd')
+    btAdd.addEventListener('click', sayHello)
+    btAdd.addEventListener('click', changeColor)
+    btAdd.addEventListener('click', addCoreWeb)
+
+    function sayHello() {
+        console.log('Hello...')
+    }
+    function changeColor(ev) {
+        const btAdd = ev.target
+        const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+        btAdd.style.backgroundColor = rndCol;
+    }
+    function addCoreWeb(){
+        const ulCoreWeb = document.querySelector('#ulCoreWeb')
+        const txtConcept = document.querySelector('#txtConcept')
+        const txt = document.createTextNode(txtConcept.value)
+        const li = document.createElement('li')
+        li.appendChild(txt)
+        ulCoreWeb.appendChild(li)
+    }
+    function random(number) {
+        return Math.floor(Math.random()*number);
+    }
+}
